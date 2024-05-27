b33e09a9d2ad5f296faba78d6faf782e5ebd594c
Bernardo Grateron <sgrateronm@gmail.com>
2024-05-27 09:05:29 -0400

    crear clase para leer txt

---
+++ src/leertxt/LeerTxt.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package leertxt;
+
+/**
+ *
+ * @author berna
+ */
+public class LeerTxt {
+    private Lista lista;
+    private int [] array;
+
+public void crearLista(){
+    
+}
+}
