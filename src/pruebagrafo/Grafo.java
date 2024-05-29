9f6220ab441c42caf266d0b1f2313aa68bc59739
ruthsenior <r.senior@correo.unimet.edu.ve>
2024-05-29 10:52:30 -0400

    dfs

--- src/pruebagrafo/Grafo.java
+++ src/pruebagrafo/Grafo.java
@@ -1,6 +1,9 @@
 
 package pruebagrafo;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *
  * @author verol
@@ -75,11 +78,37 @@ public class Grafo {
     }
     
 
-    
-    
-    
-    
-    
+ public boolean buscarPalabraDFS(String palabra) {
+        boolean[][] visitado = new boolean[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (dfs(i, j, palabra, 0, visitado)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(int i, int j, String palabra, int indice, boolean[][] visitado) {
+        if (indice == palabra.length()) {
+            return true;
+        }
+        if (i < 0 || i >= 4 || j < 0 || j >= 4 || visitado[i][j] || listaVertices[i * 4 + j].getLetra() != palabra.charAt(indice)) {
+            return false;
+        }
+        visitado[i][j] = true;
+        int[][] direcciones = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+        for (int[] d : direcciones) {
+            if (dfs(i + d[0], j + d[1], palabra, indice + 1, visitado)) {
+                return true;
+            }
+        }
+        visitado[i][j] = false;
+        return false;
+    }
+        
+   
     
 
     /**
